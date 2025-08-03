775e6b354728ca56f0d55886c0d693ef8acccfcd
Pranav <pranavkhandelwal023@gmail.com>
2025-08-03 14:54:49 +0530

    updated homepage1 with few details

---
+++ homepage1.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor =>
+    {
+anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    
+    const targetId = this.getAttribute('href');
+    if (targetId === '#') return;
+    
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+        window.scrollTo({
+            top: targetElement.offsetTop - 80,
+            behavior: 'smooth'
+        });
+                }
+            });
+        });
